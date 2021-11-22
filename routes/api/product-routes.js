f0e39d706f5e8cdb53cf37585d6c401fcc0f6d8a
Nsmall <nsmall1994@gmail.com>
2021-11-21 19:12:10 -0500

    routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,55 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  product.findAll({
+    include: [
+      {
+        model: Category,
+        attributes: ['id','catergory_name']
+      },
+      {
+        model: Tag,
+        attributes: ['id','tag_name']
+      }
+    ]
+  })
+  .then(dbReqdata => res.json(dbReqdata))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err); 
+  })
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: category,
+        attributes: ['id', 'catergory_name']
+      },
+      {
+        model: Tag,
+        attributes: ['id','tag_name']
+      }
+    ]
+  })
+    .then(dbReqdata => {
+      if (!dbReqdata) {
+        res.status(404).json({ message: 'No post found with that id' });
+        return;
+      }
+      res.json(dbReqdata);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+  })
 });
 
 // create new product
@@ -91,6 +134,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  product.destroy({
+    where: {
+      id:req.params.id
+    }
+  })
+  .then(dbReqdata => {
+    if (!dbReqdata){
+      res.status(404).json({ message:'no post has been found with that id'})
+      return;
+    }
+    res.json(dbReqdata);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err)
+  })
 });
 
 module.exports = router;
