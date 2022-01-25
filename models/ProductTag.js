9eb09a42ae51373e1b762af02fe1fcbc0e8cc9d7
Nsmall <nsmall1994@gmail.com>
2022-01-25 17:50:46 -0500

    modles updated

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,6 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
@@ -15,6 +14,7 @@ ProductTag.init(
     },
     product_id: {
       type: DataTypes.INTEGER,
+      allowNull: false,
       references: {
         model: 'product',
         key: 'id'
@@ -22,6 +22,7 @@ ProductTag.init(
     },
     tag_id: {
       type: DataTypes.INTEGER,
+      allowNull: false,
       references: {
         model: 'tag',
         key: 'id'
--- models/index.js
+++ models/index.js
@@ -3,29 +3,25 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
-const { FOREIGNKEYS } = require('sequelize/types/lib/query-types');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id'
-});
-
+})
 // Categories have many Products
-Category.hasMany(product, {
-foreignKey: 'category_id'
-});
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+})
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-through: ProductTag,
-foreignKey: 'product_id'
+  through: ProductTag,
+  foreignKey: 'product_id'
 });
-
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-through:ProductTag,
+through: ProductTag,
 foreignKey: 'tag_id'
-});
+})
 
 module.exports = {
   Product,
